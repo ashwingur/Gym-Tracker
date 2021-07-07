fc69c4c3583dd98ea3b3c1a53aa7186c3b24b49e
Ashwin <ashwingur88@gmail.com>
2021-07-07 12:00:22 +1000

    Update Exercise.java
    Created exercise comparators

--- app/src/main/java/com/example/gymtracker/Exercise.java
+++ app/src/main/java/com/example/gymtracker/Exercise.java
@@ -1,5 +1,6 @@
 package com.example.gymtracker;
 
+import java.util.Comparator;
 import java.util.Date;
 
 public class Exercise {
@@ -21,6 +22,69 @@ public class Exercise {
         this.unixTime = unixTime;
         this.date = new Date(unixTime);
     }
+
+    public static Comparator<Exercise> ExerciseDateLatestComparator = new Comparator<Exercise>() {
+        @Override
+        public int compare(Exercise e1, Exercise e2) {
+            return (int) (e1.unixTime - e2.unixTime);
+        }
+    };
+
+    public static Comparator<Exercise> ExerciseDateEarliestComparator = new Comparator<Exercise>() {
+        @Override
+        public int compare(Exercise e1, Exercise e2) {
+            return (int) (e2.unixTime - e1.unixTime);
+        }
+    };
+
+    public static Comparator<Exercise> ExerciseWeightDescendingComparator = new Comparator<Exercise>() {
+        @Override
+        public int compare(Exercise e1, Exercise e2) {
+            int result = (int) (e1.weight - e2.weight);
+            if (result == 0){
+                // If they are the same, then sort by date
+                return ExerciseDateLatestComparator.compare(e1, e2);
+            }
+            return result;
+        }
+    };
+
+    public static Comparator<Exercise> ExerciseWeightAscendingComparator = new Comparator<Exercise>() {
+        @Override
+        public int compare(Exercise e1, Exercise e2) {
+            int result = (int) (e2.weight - e1.weight);
+            if (result == 0){
+                // If they are the same, then sort by date
+                return ExerciseDateLatestComparator.compare(e1, e2);
+            }
+            return result;
+        }
+    };
+
+    public static Comparator<Exercise> ExerciseRepsAscendingComparator = new Comparator<Exercise>() {
+        @Override
+        public int compare(Exercise e1, Exercise e2) {
+            int result = (int) (e1.reps - e2.reps);
+            if (result == 0){
+                // If they are the same, then sort by date
+                return ExerciseDateLatestComparator.compare(e1, e2);
+            }
+            return result;
+        }
+    };
+
+    public static Comparator<Exercise> ExerciseRepsDescendingComparator = new Comparator<Exercise>() {
+        @Override
+        public int compare(Exercise e1, Exercise e2) {
+            int result = (int) (e2.reps - e1.reps);
+            if (result == 0){
+                // If they are the same, then sort by date
+                return ExerciseDateLatestComparator.compare(e1, e2);
+            }
+            return result;
+        }
+    };
+
 
     @Override
     public String toString() {
